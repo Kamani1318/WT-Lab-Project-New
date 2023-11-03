90e22f35028c741054d9f7f50365a62397eae124
Jake-Peralt-a <saketh.reddy6@gmail.com>
2023-11-03 11:54:15 +0530

    c

--- pdf/urls.py
+++ pdf/urls.py
@@ -21,7 +21,8 @@ urlpatterns = [
     path('pdf/display_maths/<str:success_message>/', views.display_be, name='display_be'),
     path('display/<str:success_message>/', views.display_pdf, name='display_pdf_with_message'),  # Add this line
     path('reward_selection/', views.reward_selection, name='reward_selection'),
-    path('first/', views.first_year, name='first')
+    path('first/', views.first_year, name='first'),
+    path('second/', views.second_year, name='first'),
     
 
 ]
